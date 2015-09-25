f186398c04243630362ef2e5c521e9296e312f63
Emmanuel Santos <emmanuel_92cba@hotmail.com>
2015-09-25 17:36:02 -0300

    start

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,58 +1,34 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> [-m <model>] -o <file>
+  train.py -n <n> -o <file>
   train.py -h | --help
 
 Options:
   -n <n>        Order of the model.
-  -m <model>    Model to use [default: ngram]:
-                  ngram: Unsmoothed n-grams.
-                  addone: N-grams with add-one smoothing.
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
 from docopt import docopt
 import pickle
 
-from nltk.corpus import PlaintextCorpusReader
-from nltk import RegexpTokenizer
+from nltk.corpus import gutenberg
 
-pattern = r'''(?x)    # set flag to allow verbose regexps
-([A-Z]\.)+        # abbreviations, e.g. U.S.A.
-| \w+(-\w+)*        # words with optional internal hyphens
-| \$?\d+(\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
-| \.\.\.            # ellipsis
-| [][.,;"'?():-_`]  #  
-'''
+from languagemodeling.ngram import NGram
 
-tokenizer = RegexpTokenizer(pattern)
-corpus = PlaintextCorpusReader('.', 'corpus/Harrypotter.txt', word_tokenizer = tokenizer)
-
-from languagemodeling.ngram import NGram, AddOneNGram, InterpolatedNGram, BackOffNGram
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = corpus.sents('corpus/Harrypotter.txt')
-    sents = sents[:int(0.9*len(sents))]
+    sents = gutenberg.sents('austen-emma.txt')
 
     # train the model
     n = int(opts['-n'])
-    m = opts['-m']
-    
-    if m == 'addone':
-      model = AddOneNGram(n, sents)
-    elif m == 'interpolated':
-      model = InterpolatedNGram(n, sents)
-    elif m == 'backoff':
-      model = BackOffNGram(n, sents)
-    else:
-      model = NGram(n, sents)
+    model = NGram(n, sents)
 
     # save it
     filename = opts['-o']
-    f = open('corpus/' + filename, 'wb')
+    f = open(filename, 'wb')
     pickle.dump(model, f)
     f.close()
