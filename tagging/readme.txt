7499f8dd2de4ff58608fa8baade269eb5601ef31
Emmanuel Santos <emmanuel_92cba@hotmail.com>
2015-10-03 17:38:50 -0300

    stats, falta figura jurafsky

---
+++ tagging/readme.txt
@@ -0,0 +1,21 @@
+Ejercicio 1: Corpus Ancora: Estadísticas de etiquetas POS
+
+sents: 17379
+ocurrence_words: 517300
+vocabulary_of_words: 46483
+vocabulary_of_taggeds: 48
+
+
+TAG      MEANING                    OCURRENCE_TAG    PERCENTAGE   WORDS
+
+nc       nombre común               92002            17.785038%   ['años', 'presidente', 'millones', 'equipo', 'partido']
+sp       adposición proposición     79904            15.446356%   ['de', 'en', 'a', 'del', 'con']
+da       determinante artículo      54552            10.545525%   ['la', 'el', 'los', 'las', 'El']
+vm       verbo principal            50609             9.783298%   ['está', 'tiene', 'dijo', 'puede', 'hace']
+aq       adjetivo calificativo      33904             6.554031%   ['pasado', 'gran', 'mayor', 'nuevo', 'próximo']
+fc       puntuación coma            30148             5.827953%   [',']
+np       nombre propio              29113             5.627876%   ['Gobierno', 'España', 'PP', 'Barcelona', 'Madrid']
+fp       puntuación (.)             21157             4.089890%   ['.', '(', ')']
+rg       adverbio general           15333             2.964044%   ['más', 'hoy', 'también', 'ayer', 'ya']
+cc       conjunción coordinada      15023             2.904118%   ['y', 'pero', 'o', 'Pero', 'e']
+
--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -9,15 +9,59 @@ Options:
 """
 from docopt import docopt
 
-from corpus.ancora import SimpleAncoraCorpusReader
+from corpus.ancora import SimpleAncoraCorpusReader, AncoraCorpusReader
 
+from collections import defaultdict
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
+    # c = AncoraCorpusReader('ancora/ancora-2.0')
+    # a, b = zip(*c.tagged_words())
+    # voc = len(set(b))
+    # print('voc_tag: {}'.format(voc))
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/')
     sents = corpus.tagged_sents()
+    tagged_words = corpus.tagged_words()
+    
+    words, taggeds = zip(*tagged_words)
 
+    ocurrence_words = len(words)
+    voc_words = len(set(words))
+    voc_taggeds = len(set(taggeds))
+    most_repeat = []
+
+    ocurrence_taggeds = defaultdict(int) 
+    words_for_tag  = defaultdict(int) 
+    for word, tagged in tagged_words:
+        ocurrence_taggeds[tagged] += 1
+        words_for_tag[(tagged, word)] += 1 
+
+    ocurrence_taggeds = sorted(ocurrence_taggeds.items(), key=lambda o_tag: 
+                                -o_tag[1])
+    words_for_tag = sorted(words_for_tag.items(), key=lambda w_tag: -w_tag[1])
+    
     # compute the statistics
     print('sents: {}'.format(len(sents)))
+    print('ocurrence_words: {}'.format(ocurrence_words))
+    print('vocabulary_of_words: {}'.format(voc_words))
+    print('vocabulary_of_taggeds: {}'.format(voc_taggeds))
+
+    print('\n\ntagged   ocurrence_tagged    percentage   five_words\n')
+    for x in range(10):
+        tag = ocurrence_taggeds[x][0]
+        o_tag = ocurrence_taggeds[x][1]
+        percentage = 100 * (ocurrence_taggeds[x][1] / ocurrence_words)
+        words_tag = []
+        for i in range(len(words_for_tag)):
+            if words_for_tag[i][0][0] == tag:
+                words_tag += [words_for_tag[i][0][1]]
+            if len(words_tag) == 5:
+                break
+        print('{0:6}   {1:16d}   {2:10f}%   {3}'.format(tag, o_tag, percentage,
+    
+
+
+
+                                                           words_tag))
