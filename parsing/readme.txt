8b5cf8f629a331fd2fe5091045e2ed5537d0bd1a
Emmanuel Santos <emmanuel_92cba@hotmail.com>
2015-10-28 13:14:22 -0300

    eval listo

---
+++ parsing/readme.txt
@@ -0,0 +1,36 @@
+#Ejercicio 1: Evaluación de Parsers
+
+##Baseline Flat:
+
+
+          | Precision | Recall | F1
+---------------------------------------
+Labeled   |99.93%     |14.57%  |25.43% 
+UnLabeled |100.00%     |14.58%  |25.45% 
+
+real	0m8.269s
+user	0m8.181s
+sys	0m0.072s
+
+
+##Baseline rbranch:
+
+          | Precision | Recall | F1
+---------------------------------------
+Labeled   |8.81%     |14.57%  |10.98% 
+UnLabeled |8.87%     |14.68%  |11.06% 
+
+real	0m9.274s
+user	0m9.160s
+sys	0m0.100s
+
+##Baseline lbranch:
+
+          | Precision | Recall | F1
+---------------------------------------
+Labeled   |8.81%     |14.57%  |10.98% 
+UnLabeled |14.71%     |24.33%  |18.33% 
+
+real	0m9.158s
+user	0m9.013s
+sys	0m0.132s
--- parsing/scripts/eval.py
+++ parsing/scripts/eval.py
@@ -1,11 +1,13 @@
 """Evaulate a parser.
 
 Usage:
-  eval.py -i <file>
+  eval.py -i <file> [-m <m>] [-n <n>]
   eval.py -h | --help
 
 Options:
   -i <file>     Parsing model file.
+  -m <m>        Only long sentences less than or equal to m.
+  -n <n>        Only the first n sentences.
   -h --help     Show this screen.
 """
 from docopt import docopt
@@ -34,39 +36,73 @@ if __name__ == '__main__':
     model = pickle.load(f)
     f.close()
 
+    n = opts['-n']
+    m = opts['-m']
+    if n != None:
+        n = int(n)
+    if m != None:
+        m = int(m)
+
     print('Loading corpus...')
     files = '3LB-CAST/.*\.tbf\.xml'
     corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/', files)
     parsed_sents = list(corpus.parsed_sents())
 
     print('Parsing...')
-    hits, total_gold, total_model = 0, 0, 0
-    n = len(parsed_sents)
-    format_str = '{:3.1f}% ({}/{}) (P={:2.2f}%, R={:2.2f}%, F1={:2.2f}%)'
-    progress(format_str.format(0.0, 0, n, 0.0, 0.0, 0.0))
-    for i, gold_parsed_sent in enumerate(parsed_sents):
+    hits, total_gold, total_model = 0, 0, 0 
+    u_hits, u_total_gold, u_total_model = 0, 0, 0
+    if n == None:
+        n = len(parsed_sents)
+    format_str = ('{:3.1f}% ({}/{}) Label: (P={:2.2f}%, R={:2.2f}%, F1={:2.2f}%)'
+    ' UnLabel: (P={:2.2f}%, R={:2.2f}%, F1={:2.2f}%)')
+    progress(format_str.format(0.0, 0, n, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+    for i, gold_parsed_sent in enumerate(parsed_sents[:n]):
         tagged_sent = gold_parsed_sent.pos()
+        if m == None or len(tagged_sent) <= m:
 
-        # parse
-        model_parsed_sent = model.parse(tagged_sent)
+            # parse
+            model_parsed_sent = model.parse(tagged_sent)
 
-        # compute labeled scores
-        gold_spans = spans(gold_parsed_sent, unary=False)
-        model_spans = spans(model_parsed_sent, unary=False)
-        hits += len(gold_spans & model_spans)
-        total_gold += len(gold_spans)
-        total_model += len(model_spans)
+            # compute labeled scores
+            gold_spans = spans(gold_parsed_sent, unary=False)
+            model_spans = spans(model_parsed_sent, unary=False)
+            hits += len(gold_spans & model_spans)
+            total_gold += len(gold_spans)
+            total_model += len(model_spans)
 
-        # compute labeled partial results
-        prec = float(hits) / total_model * 100
-        rec = float(hits) / total_gold * 100
-        f1 = 2 * prec * rec / (prec + rec)
+            # compute labeled partial results
+            prec = float(hits) / total_model * 100
+            rec = float(hits) / total_gold * 100
+            f1 = 2 * prec * rec / (prec + rec)
 
-        progress(format_str.format(float(i+1) * 100 / n, i+1, n, prec, rec, f1))
+            # compute unlabeled scores
+            s, x, y = zip(*gold_spans)
+            u_gold_spans = set(zip(x,y))
+            s, x, y = zip(*model_spans)
+            u_model_spans = set(zip(x,y))
+            u_hits += len(u_gold_spans & u_model_spans)
+            u_total_gold += len(u_gold_spans)
+            u_total_model += len(u_model_spans)
+
+            # compute unlabeled partial results
+            u_prec = float(u_hits) / u_total_model * 100
+            u_rec = float(u_hits) / u_total_gold * 100
+            u_f1 = 2 * u_prec * u_rec / (u_prec + u_rec)
+
+
+            progress(format_str.format(float(i+1) * 100 / n, i+1, n, prec, 
+                                        rec, f1, u_prec, u_rec, u_f1))
 
     print('')
     print('Parsed {} sentences'.format(n))
-    print('Labeled')
-    print('  Precision: {:2.2f}% '.format(prec))
-    print('  Recall: {:2.2f}% '.format(rec))
-    print('  F1: {:2.2f}% '.format(f1))
+    
+    print('          | Precision | Recall | F1')
+    print('---------------------------------------')
+    print('Labeled   |', end='')
+    print('{:2.2f}%     |'.format(prec), end='')
+    print('{:2.2f}%  |'.format(rec), end='')
+    print('{:2.2f}% '.format(f1))
+    print('UnLabeled |', end='')
+    print('{:2.2f}%     |'.format(u_prec), end='')
+    print('{:2.2f}%  |'.format(u_rec), end='')
+    print('{:2.2f}% '.format(u_f1))
