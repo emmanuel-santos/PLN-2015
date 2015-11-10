6abd25313fc844b8ee4c2d401a4a2e0ff1904591
Emmanuel Santos <emmanuel_92cba@hotmail.com>
2015-11-10 00:36:44 -0300

    correcion en la optimizacion de cky

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -10,13 +10,17 @@ class CKYParser:
         grammar -- a binarised NLTK PCFG.
         """
         self.grammar = grammar
-        self.rproduction = rproduction = defaultdict(list)
-      
+        self.prod_nonlexical = prod_nonlexical = defaultdict(list)
+        self.prod_lexical = prod_lexical = defaultdict(list)
+
         for production in grammar.productions():
             if production.is_nonlexical():
-                r0 = str(production.rhs()[0])
-                r1 = str(production.rhs()[1])
-                rproduction[(r0, r1)] += [production]
+                r0 = repr(production.rhs()[0])
+                r1 = repr(production.rhs()[1])
+                prod_nonlexical[(r0, r1)] += [production]
+            else:
+                r = production.rhs()[0]
+                prod_lexical[r] += [production]
 
         self._pi = {}
         self._bp = {}
@@ -26,26 +30,24 @@ class CKYParser:
 
         sent -- the sequence of terminals.
         """
-        productions = self.grammar.productions()
-        start = str(self.grammar.start())
-        rprod = self.rproduction
+        start = repr(self.grammar.start())
+        p_nonlex = self.prod_nonlexical
+        p_lex = self.prod_lexical
         self._pi = pi = dict()
         self._bp = bp = dict()
         lp = 0
         t = None
 
-        j = 1
         # inicializacion
+        j = 0
         for word in sent:
-            for production in productions:
-                leaves = production.rhs()
-                if leaves[0] == word:
-                    pi[(j, j)] = {}
-                    bp[(j, j)] = {}
-                    Nonterminal = str(production.lhs())
-                    pi[(j, j)][Nonterminal] = log2(production.prob())
-                    bp[(j, j)][Nonterminal] = Tree(Nonterminal, [word])
-                    j += 1
+            j += 1
+            pi[(j, j)] = {}
+            bp[(j, j)] = {}
+            for production in p_lex[word]:
+                Nonterminal = repr(production.lhs())
+                pi[(j, j)][Nonterminal] = log2(production.prob())
+                bp[(j, j)][Nonterminal] = Tree(Nonterminal, [word])
 
         # cky
         for i in range(1, len(sent)):
@@ -58,8 +60,8 @@ class CKYParser:
                         for r1 in pi[((k + 1), l)]:
                             lp1 = pi[(j, k)][r0]
                             lp2 = pi[((k + 1), l)][r1]
-                            for production in rprod[(r0, r1)]:
-                                Nonterminal = str(production.lhs())
+                            for production in p_nonlex[(r0, r1)]:
+                                Nonterminal = repr(production.lhs())
                                 prob = log2(production.prob())
                                 prob = prob + lp1 + lp2
                                 if (Nonterminal not in pi[(j, l)] or
@@ -71,9 +73,9 @@ class CKYParser:
                                     tree = Tree(Nonterminal, [left, right])
                                     bp[(j, l)][Nonterminal] = tree
 
-        # print(start)
-        # print(pi[(1,len(sent))])
-        if str(start) in pi[(1, len(sent))]:
+        # print('\n ' + start + '\n')
+        # print(pi[(1, len(sent))])
+        if start in pi[(1, len(sent))]:
             lp = pi[(1, len(sent))][start]
             t = bp[(1, len(sent))][start]
 
